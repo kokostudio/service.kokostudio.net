929ad7c0a720af3fce605808459a247107c6f042
kokostudio <kokostudio@hotmail.com>
2019-01-19 14:07:34 +0700

    Update category.php

--- category.php
+++ category.php
@@ -366,7 +366,7 @@
     </div>
   </div>
 
-  
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="node_modules/jquery/dist/jquery.min.js"></script>
   <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
